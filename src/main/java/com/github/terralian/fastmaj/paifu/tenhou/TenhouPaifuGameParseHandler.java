1b7f9b5c59b2accca5d503a1d6914070282fed30
terra.lian <630985301@qq.com>
2023-03-26 21:29:32 +0800

    完善牌谱解析功能

--- src/main/java/com/github/terralian/fastmaj/paifu/tenhou/TenhouPaifuGameParseHandler.java
+++ src/main/java/com/github/terralian/fastmaj/paifu/tenhou/TenhouPaifuGameParseHandler.java
@@ -2,17 +2,14 @@ package com.github.terralian.fastmaj.paifu.tenhou;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.github.terralian.fastmaj.encode.Encode34;
 import com.github.terralian.fastmaj.game.KazeEnum;
-import com.github.terralian.fastmaj.game.event.DrawEvent;
-import com.github.terralian.fastmaj.game.event.river.ChiiEvent;
-import com.github.terralian.fastmaj.game.event.river.PonEvent;
-import com.github.terralian.fastmaj.game.event.tehai.AnnkanEvent;
-import com.github.terralian.fastmaj.game.event.tehai.KiriEvent;
 import com.github.terralian.fastmaj.hai.HaiPool;
-import com.github.terralian.fastmaj.hai.IHai;
 import com.github.terralian.fastmaj.paifu.domain.PaifuGame;
 import com.github.terralian.fastmaj.paifu.domain.PaifuKyoku;
+import com.github.terralian.fastmaj.tehai.ITehai;
 
 /**
  * 天凤牌谱解析器实例，用于将牌谱解析为{@link PaifuGame}
@@ -48,14 +45,14 @@ public class TenhouPaifuGameParseHandler implements ITenhouPaifuParseHandler {
 
     @Override
     public void startGame(boolean isSanma, int taku, boolean isTonnan, boolean isSoku, boolean isUseAka, boolean isAriAri,
-            String[] playerNames, int[] playerRates, String[] playerDans) {
-        paifuGame.setRoom(TenhouPaifuStringPool.TAKU[taku])  //
+                          String[] playerNames, int[] playerRates, String[] playerDans) {
+        paifuGame.setRoom(TenhouPaifuStringPool.TAKU[taku]) //
                 .setPlatform(TenhouPaifuStringPool.PLATFORM) //
                 .setPlayerSize(isSanma ? 3 : 4) //
                 .setEndBakaze(isTonnan ? KazeEnum.NAN : KazeEnum.DON) //
                 .setUseRed(isUseAka) //
                 .setPlayerNames(playerNames) //
-                .setKyokus(new ArrayList<>()); //
+                .setKyokus(new ArrayList<>());
     }
 
     @Override
@@ -65,15 +62,20 @@ public class TenhouPaifuGameParseHandler implements ITenhouPaifuParseHandler {
 
     @Override
     public void startKyoku(int[] playerPoints, List<List<Integer>> playerHaipais, int oya, int bakaze, int kyoku, int honba, int kyotaku,
-            int firstDoraDisplay, int[] yama) {
+                           int firstDoraDisplay, int[] yama) {
+        List<ITehai> startTehais = playerHaipais.stream() //
+                .map(Encode34::toTehai) //
+                .collect(Collectors.toList());
+
         currentKyoku = new PaifuKyoku();
-        currentKyoku.setStartPoints(playerPoints) //
-                .setOya(oya) //
-                .setBakaze(KazeEnum.getByOrder(bakaze)) //
-                .setHonba(honba) //
+        currentKyoku.setStartPoints(playerPoints)//
+                .setOya(oya)//
+                .setBakaze(KazeEnum.getByOrder(bakaze))//
+                .setHonba(honba)//
                 .setKyotaku(kyotaku) //
                 .setYamas(yama) //
-                .setActions(new ArrayList<>());
+                .setStartTehais(startTehais) //
+                .setFirstDoraDisplay(HaiPool.getById(firstDoraDisplay));
     }
 
     @Override
@@ -82,76 +84,9 @@ public class TenhouPaifuGameParseHandler implements ITenhouPaifuParseHandler {
         currentKyoku = null;
     }
 
-    @Override
-    public void draw(int position, int tsumoHai) {
-        DrawEvent drawEvent = new DrawEvent() //
-                .setDrawHai(getHaiById(tsumoHai)) //
-                .setPosition(position);
-        // TODO
-        currentKyoku.getActions().add(drawEvent);
-    }
-
-    @Override
-    public void kiri(int position, int kiriHai) {
-        KiriEvent kiriEvent = new KiriEvent() //
-                .setKiriHai(getHaiById(kiriHai)) //
-                .setPosition(position); //
-        currentKyoku.getActions().add(kiriEvent);
-    }
-
-    @Override
-    public void chi(int position, int from, int[] selfHai, int nakiHai) {
-        ChiiEvent chiiEvent = new ChiiEvent() //
-                .setFrom(from) //
-                .setPosition(position) //
-                .setSelfHais(getHaiByIds(selfHai)) //
-                .setNakiHai(getHaiById(nakiHai)); //
-        currentKyoku.getActions().add(chiiEvent);
-    }
-
-    @Override
-    public void pon(int position, int from, int[] selfHai, int nakiHai) {
-        PonEvent ponEvent = new PonEvent() //
-                .setFrom(from) //
-                .setPosition(position) //
-                .setSelfHais(getHaiByIds(selfHai)) //
-                .setNakiHai(getHaiById(nakiHai)); //
-        currentKyoku.getActions().add(ponEvent);
-    }
-
-    @Override
-    public void annkan(int position, int[] selfHai) {
-        AnnkanEvent annkanEvent = new AnnkanEvent() //
-                .setPosition(position) //
-                .setAnnkanHai(getHaiByIds(selfHai)); //
-        currentKyoku.getActions().add(annkanEvent);
-    }
-
     @SuppressWarnings("unchecked")
     @Override
     public PaifuGame getParseData() {
         return paifuGame;
-    }
-
-    /**
-     * 根据id编码获取牌对象
-     *
-     * @param hai 牌id
-     */
-    private IHai getHaiById(int hai) {
-        return HaiPool.getById(hai, paifuGame.isUseRed(), 1);
-    }
-
-    /**
-     * 根据id编码获取牌数组对象
-     *
-     * @param hais 牌id
-     */
-    private IHai[] getHaiByIds(int[] hais) {
-        IHai[] h = new IHai[hais.length];
-        for (int i = 0; i < hais.length; i++) {
-            h[i] = getHaiById(hais[i]);
-        }
-        return h;
     }
 }

fdceb14a86eaa3cb9de98e3665ac982f46a9935b
terra.lian <630985301@qq.com>
2023-04-13 23:01:52 +0800

    TehaiBuilder改写历史测试用例，以进行代码测试

--- src/test/java/com/github/terralian/fastmaj/test/agari/FuCalculatorTest.java
+++ src/test/java/com/github/terralian/fastmaj/test/agari/FuCalculatorTest.java
@@ -1,11 +1,6 @@
 package com.github.terralian.fastmaj.test.agari;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
 
 import com.github.terralian.fastmaj.agari.DivideInfo;
 import com.github.terralian.fastmaj.agari.FuCalculator;
@@ -14,14 +9,17 @@ import com.github.terralian.fastmaj.agari.ITehaiAgariDivider;
 import com.github.terralian.fastmaj.encode.EncodeMark;
 import com.github.terralian.fastmaj.game.KazeEnum;
 import com.github.terralian.fastmaj.game.context.PlayerGameContext;
-import com.github.terralian.fastmaj.hai.HaiPool;
-import com.github.terralian.fastmaj.player.RivalEnum;
 import com.github.terralian.fastmaj.tehai.ITehai;
+import com.github.terralian.fastmaj.tehai.TehaiBuilder;
 import com.github.terralian.fastmaj.third.mjscore.MjscoreAdapter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * {@link FuCalculator}单元测试
- * 
+ *
  * @author terra.lian
  */
 public class FuCalculatorTest {
@@ -76,32 +74,35 @@ public class FuCalculatorTest {
         assertEquals(40, fu);
 
         // 40符
-        tehai = EncodeMark.toTehai("234m1p555z57s78p");
-        tehai.chii(HaiPool.p(9), HaiPool.p(7), HaiPool.p(8));
-        tehai.chii(HaiPool.s(6), HaiPool.s(5), HaiPool.s(7));
-        tehai.minkan(HaiPool.z(5), RivalEnum.BOTTOM);
-        tehai.draw(HaiPool.p(1));
+        tehai = TehaiBuilder.from("234m", "1p") //
+                .addChi("9s", "78s") //
+                .addChi("6s", "57s") //
+                .addMinkan("5z") //
+                .addOne("1p") //
+                .get();
         divideInfos = agariDivider.divide(tehai);
         context.setRon(false);
         fu = fuCalculator.compute(tehai, tehai.getDrawHai(), divideInfos, context);
         assertEquals(40, fu);
 
         // 30符
-        tehai = EncodeMark.toTehai("34m11s06m1144z");
-        tehai.pon(HaiPool.z(4), RivalEnum.TOP);
-        tehai.pon(HaiPool.z(1), RivalEnum.TOP);
-        tehai.chii(HaiPool.m(4), HaiPool.m(0), HaiPool.m(6));
-        tehai.draw(HaiPool.m(5));
+        tehai = TehaiBuilder.from("34m11s06m1144z") //
+                .pon("4z") //
+                .pon("1z") //
+                .chi("4m", "06m") //
+                .addOne("5m") //
+                .get();
         divideInfos = agariDivider.divide(tehai);
         context.setRon(false);
         fu = fuCalculator.compute(tehai, tehai.getDrawHai(), divideInfos, context);
         assertEquals(30, fu);
 
         // 双风跳符，40符
-        tehai = EncodeMark.toTehai("12367m114477z");
-        tehai.pon(HaiPool.z(4), RivalEnum.TOP);
-        tehai.pon(HaiPool.z(7), RivalEnum.TOP);
-        tehai.draw(HaiPool.m(8));
+        tehai = TehaiBuilder.from(EncodeMark.toTehai("12367m114477z")) //
+                .pon("4z") //
+                .pon("7z") //
+                .addOne("8m") //
+                .get();
         divideInfos = agariDivider.divide(tehai);
         context.setRon(true);
         context.setBakaze(KazeEnum.DON);
